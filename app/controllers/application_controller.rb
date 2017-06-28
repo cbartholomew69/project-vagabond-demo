292bfedaccc6d1586f8ed58c46746fc2fd092dbc
MonicaGrages <mgrages1@gmail.com>
2017-06-27 21:42:51 -0400

    created user model with devise and set up some authorizations with cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include CanCan::ControllerAdditions
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:notice] = "Access denied!"
+    flash.keep(:notice)
+    redirect_to root_url
+  end
+
   def home
     render 'cities/home'
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource  only: [:edit, :update, :destroy]
 
   def show
     @post = Post.find(params[:id])
@@ -10,9 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    puts "request.body is: "
-    puts request.body
-    @post = Post.create(city_id: request.params["city_id"], title: request.params["post"]["title"], content: request.params["post"]["content"], photo: request.params["post"]["photo"])
+    @post = Post.create(city_id: request.params["city_id"], user_id: current_user.id, title: request.params["post"]["title"], content: request.params["post"]["content"], photo: request.params["post"]["photo"])
     redirect_to "/cities/#{@post.city_id}/posts/#{@post.id}"
   end
 
