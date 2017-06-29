2cb2068255bc3971a6747106937c173318051c97
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-06-29 07:08:21 -0400

    cities new controller action works

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -15,5 +15,26 @@ class CitiesController < ApplicationController
     @posts = @city.posts.order("created_at DESC")
     puts @posts
   end
+   def create
+    @city = City.new(params[:id])
+    if @city.save
 
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @city
+    else
+      render 'new'
+    end
+  end
 end
+  def destroy
+    @city = City.find(params[:id])
+
+  if city.destroy
+     redirect_to post_path(@comment.post_id),
+                notice: 'City post successfully destroyed.'
+  else
+    redirect_to post_path(@city.post_id),
+                alert: 'Error destroying comment.'
+  end
+end
+
