9f3619987402df517f942eb55678f2355eb8a00c
MonicaGrages <mgrages1@gmail.com>
2017-06-28 22:59:23 -0400

    link to a user show page is working. no real content on user show page yet.

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,7 @@ Rails.application.routes.draw do
 
   root 'application#home'
 
-# get 'cities/:city_id/posts/new', to: 'posts#new', as: 'post_new'
-
-# post 'cities/:city_id/posts', to: 'posts#create', as: 'post_create'
-
-# get '/cities/:id', to: 'cities#show', as: 'city'
-
-# get 'cities/:id/posts/:id', to: 'posts#show', as: 'post'
+  get 'users/:id', to: 'users#show', as: 'user'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
