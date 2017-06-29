08a337e8e0b1d48c2e403b114ef588035ed6c1c2
hellajeffrey <Jeffreyhenneberg@gmail.com>
2017-06-29 11:43:28 -0400

    added validation for post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :city
   belongs_to :user
+
+attr_accessor :title, :content
+
+  validates :title, :content, :presence => true
+  validates :title, :length => { :minimum => 1 }
+  validates :title, :length => { :maximum => 200 }
 end
