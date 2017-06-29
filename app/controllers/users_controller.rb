ccfa54daff9d39ac1b101b00280ee4e86f60b938
MonicaGrages <mgrages1@gmail.com>
2017-06-29 07:46:38 -0400

    added content to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,10 @@ class UsersController < ApplicationController
     def show
       @user = User.find(params[:id])
       @posts = @user.posts.order("created_at DESC")
+      @cities_contributed_to = @posts.map do |post|
+        post.city
+      end
+      @cities_contributed_to.uniq!
+      @contributions = @cities_contributed_to.count
     end
 end
